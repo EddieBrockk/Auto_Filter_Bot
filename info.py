1c826af713e626c45c72e17c32ba2c6af3992ad3
Eddie Brock <158460213+EddieBrockk@users.noreply.github.com>
2025-11-22 13:46:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/sonchar_backup')  # Sup
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1001645171512")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1001645171512")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1001696292306")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
@@ -120,7 +120,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/sonchar_backup') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+aW9Az0zfe2AyNmJl') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/wolfmasterxd') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/sonchar_backup') # Update channel link for the bot
 
